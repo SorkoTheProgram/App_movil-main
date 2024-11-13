7e2180e7145f6c13227f52cb14bc2901c2758062
SorkoTallBoy <ig.sorko@duocuc.cl>
2024-11-13 18:25:13 -0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,25 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBckMIJotq6y859L9MQm8bn1nR6JXsfni0",
+    authDomain: "sorkoapp.firebaseapp.com",
+    projectId: "sorkoapp",
+    storageBucket: "sorkoapp.firebasestorage.app",
+    messagingSenderId: "145819077989",
+    appId: "1:145819077989:web:f6d1de6952b8628c85258c"
+  }
 };
 
+// Initialize Firebase
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBckMIJotq6y859L9MQm8bn1nR6JXsfni0",
+    authDomain: "sorkoapp.firebaseapp.com",
+    projectId: "sorkoapp",
+    storageBucket: "sorkoapp.firebasestorage.app",
+    messagingSenderId: "145819077989",
+    appId: "1:145819077989:web:f6d1de6952b8628c85258c"
+  };
