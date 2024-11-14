7835772b97a175b734e49669cfa94f9f63e2722c
SorkoTallBoy <ig.sorko@duocuc.cl>
2024-11-14 06:09:37 -0300

    51% bloqueo de menu

--- src/app/pages/inicio-sesion/inicio-sesion.page.ts
+++ src/app/pages/inicio-sesion/inicio-sesion.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { UtilsService } from '../../services/utils.service';
+import { MenuController } from '@ionic/angular'; // Importación de MenuController
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -12,7 +13,8 @@ export class InicioSesionPage implements OnInit {
   // Constructor con inyección de servicios
   constructor(
     private utils: UtilsService,
-    private authSvc: AuthService
+    private authSvc: AuthService,
+    private menuCtrl: MenuController // Inyección de MenuController
   ) {}
 
   // Formulario de acceso con validaciones
@@ -21,7 +23,18 @@ export class InicioSesionPage implements OnInit {
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
 
+  // Método requerido por la interfaz OnInit
   ngOnInit() {}
+
+  ionViewWillEnter() {
+    // Desactiva el menú cada vez que la página se muestra
+    this.menuCtrl.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // Activa el menú al salir de la página de inicio de sesión
+    this.menuCtrl.enable(true);
+  }
 
   // Método para iniciar sesión
   async acceder() {
