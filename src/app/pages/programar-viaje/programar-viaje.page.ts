0f820fa9b96af671b25637f5fa58528d332923d1
SorkoTallBoy <ig.sorko@duocuc.cl>
2024-12-15 18:31:17 -0300

    cambio de "fin" a "destino"

--- src/app/pages/programar-viaje/programar-viaje.page.ts
+++ src/app/pages/programar-viaje/programar-viaje.page.ts
@@ -4,8 +4,8 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { Viaje } from 'src/app/models/models';
-import { Usuario } from 'src/app/models/models'; // Importa el tipo Usuario
-import { MapboxService } from 'src/app/services/mapbox.service'; // Importa el servicio
+import { Usuario } from 'src/app/models/models';
+import { MapboxService } from 'src/app/services/mapbox.service';
 import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
 @Component({
@@ -16,16 +16,16 @@ import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 export class ProgramarViajePage implements OnInit {
   viajeForm!: FormGroup;
   conductorId: string = '';
-  creadorEmail: string = ''; 
-  conductorNombreCompleto: string = ''; // Aquí guardamos el nombre y apellido concatenados
-  minFecha: string = ''; 
+  creadorEmail: string = '';
+  conductorNombreCompleto: string = '';
+  minFecha: string = '';
   coordenadas: { latitud: number; longitud: number } = { latitud: 0, longitud: 0 };
-  
+
   // Búsqueda de direcciones
   direccionInicio: string = '';
-  direccionFin: string = '';
+  direccionDestino: string = '';
   searchResultsInicio: any[] = [];
-  searchResultsFin: any[] = [];
+  searchResultsDestino: any[] = [];
   searchSubject: Subject<{ query: string; tipo: string }> = new Subject();
 
   constructor(
@@ -33,7 +33,7 @@ export class ProgramarViajePage implements OnInit {
     private fb: FormBuilder,
     private firestore: AngularFirestore,
     private afAuth: AngularFireAuth,
-    private mapboxService: MapboxService // Inyecta el servicio de Mapbox
+    private mapboxService: MapboxService
   ) {}
 
   ngOnInit() {
@@ -42,7 +42,7 @@ export class ProgramarViajePage implements OnInit {
     // Configurar formulario reactivo
     this.viajeForm = this.fb.group({
       inicio: ['', Validators.required],
-      fin: ['', Validators.required],
+      destino: ['', Validators.required],
       fecha: ['', Validators.required],
       precio: [null, [Validators.required, Validators.min(0)]],
       asientos: [null, [Validators.required, Validators.min(1)]],
@@ -51,7 +51,6 @@ export class ProgramarViajePage implements OnInit {
     });
 
     // Obtener el ID y email del usuario autenticado (conductor)
-    // Obtener datos del usuario autenticado
     this.afAuth.onAuthStateChanged((user) => {
       if (user) {
         this.conductorId = user.uid;
@@ -79,9 +78,6 @@ export class ProgramarViajePage implements OnInit {
       this.buscarDireccionMapbox(query, tipo);
     });
 
-    
-
-    // Cargar las coordenadas desde localStorage
     this.cargarCoordenadas();
   }
 
@@ -94,43 +90,39 @@ export class ProgramarViajePage implements OnInit {
     }
   }
 
-  // Función para buscar la dirección con Mapbox
   buscarDireccionMapbox(query: string, tipo: string) {
     const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=pk.eyJ1IjoiY3Jpc3RpYW5kdW9jIiwiYSI6ImNtNGFkZThpODA2dnYycXB0YXVja3oxYmMifQ.SKCcsYUVVVDkgXjf6MjdTA`;
 
     fetch(mapboxUrl)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const results = data.features;
         if (tipo === 'inicio') {
           this.searchResultsInicio = results;
-        } else if (tipo === 'fin') {
-          this.searchResultsFin = results;
+        } else if (tipo === 'destino') {
+          this.searchResultsDestino = results;
         }
       })
-      .catch(error => console.error('Error al obtener la dirección de Mapbox:', error));
+      .catch((error) => console.error('Error al obtener la dirección de Mapbox:', error));
   }
 
-  // Actualiza el resultado seleccionado
   onSelectResult(result: any, tipo: string) {
     const coords = result.geometry.coordinates;
-    this.coordenadas = { latitud: coords[1], longitud: coords[0] }; // Asegúrate de que estas coordenadas sean correctas
-    
+    this.coordenadas = { latitud: coords[1], longitud: coords[0] };
+
     if (tipo === 'inicio') {
       this.direccionInicio = result.place_name;
       this.viajeForm.get('inicio')?.setValue(this.direccionInicio);
       this.searchResultsInicio = [];
-    } else if (tipo === 'fin') {
-      this.direccionFin = result.place_name;
-      this.viajeForm.get('fin')?.setValue(this.direccionFin);
-      this.searchResultsFin = [];
+    } else if (tipo === 'destino') {
+      this.direccionDestino = result.place_name;
+      this.viajeForm.get('destino')?.setValue(this.direccionDestino);
+      this.searchResultsDestino = [];
     }
-  
-    // Guardar las coordenadas en localStorage
+
     localStorage.setItem('coordenadasViaje', JSON.stringify(this.coordenadas));
   }
 
-  // Detectar búsqueda en tiempo real
   onSearch(event: any, tipo: string) {
     const query = event.target.value;
 
@@ -139,8 +131,8 @@ export class ProgramarViajePage implements OnInit {
     } else {
       if (tipo === 'inicio') {
         this.searchResultsInicio = [];
-      } else if (tipo === 'fin') {
-        this.searchResultsFin = [];
+      } else if (tipo === 'destino') {
+        this.searchResultsDestino = [];
       }
     }
   }
@@ -163,13 +155,11 @@ export class ProgramarViajePage implements OnInit {
       coordenadas: this.coordenadas,
     };
 
-    // Guardar en Firestore
     this.firestore
       .collection('viajes')
       .add(viaje)
       .then(() => {
         console.log('Viaje programado exitosamente');
-        // Guardar en localStorage
         let viajesProgramados = JSON.parse(localStorage.getItem('viajesProgramados') || '[]');
         viajesProgramados.push(viaje);
         localStorage.setItem('viajesProgramados', JSON.stringify(viajesProgramados));
@@ -180,6 +170,3 @@ export class ProgramarViajePage implements OnInit {
       });
   }
 }
-
-
-
